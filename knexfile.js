29cee5b8b5e7b2fcaee1ad919b1de2a3d605a281
Laura Courtney <lauraelainecourtney@gmail.com>
2025-02-05 12:13:27 -0400

    initialized knex, configured with MySQL2, set up env variables

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST || "127.0.0.1",
+      user: process.env.DB_USERNAME || "waresync_user",
+      password: process.env.DB_PASSWORD || "waresyncpassword",
+      database: process.env.DB_DATABASE || "waresync_backend_db",
+    },
+    migrations: {
+      directory: "./src/migrations",
+    },
+    seeds: {
+      directory: "./src/seeds",
+    },
+  },
+};
